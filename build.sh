7bbd0a0ce6fe3779246493d43210a3a2cff4d9b3
dknb0709 <dknb0709@github.com>
2022-10-24 08:23:45 +0900

    init

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ROOT_DIR=`pwd`
+TEST_DIR=${ROOT_DIR}/test
+BUILD_DIR=${ROOT_DIR}/build
+
+function clean {
+  echo "-- Clean build directory"
+  rm -rf $BUILD_DIR 2> /dev/null
+}
+
+function build {
+  echo "-- Build start"
+  mkdir $BUILD_DIR && cd $BUILD_DIR
+  cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+  make
+  if [ ! $? -eq 0 ]; then
+    echo "[Error] build failed"
+    exit 1
+  fi
+}
+
+function run {
+  echo "-- Running program"
+  ${BUILD_DIR}/src/main.bin
+}
+
+
+clean
+build
+run
+
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int square(int n) { return n * n; }
+
+int main(int argc, char* argv[]) {
+  std::cout << square(3) << std::endl;
+  return 0;
+}
