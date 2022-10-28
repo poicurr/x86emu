337c8c0933da7b623e13b6767fc6f366c2c6ac83
dknb0709 <dknb0709@github.com>
2022-10-28 14:37:50 +0900

    section 2.3

--- include/Emulator.hpp
+++ include/Emulator.hpp
@@ -2,10 +2,14 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <cstdio>
 #include <cstring>
+#include <functional>
+#include <vector>
 
-static const size_t REGISTER_COUNT = 24;
-static const size_t ESP = 0;
+enum Register { EAX, ECX, EDX, EBX, ESP, EBP, ESI, EDI, REGISTERS_COUNT };
+const char* registers_name[] = {"EAX", "ECX", "EDX", "EBX",
+                                "ESP", "EBP", "ESI", "EDI"};
 
 struct Emulator {
   Emulator(size_t size, uint32_t eip, uint32_t esp) {
@@ -14,10 +18,50 @@ struct Emulator {
     eip = eip;
     registers[ESP] = esp;
   }
-
   virtual ~Emulator() { delete memory; }
 
-  uint32_t registers[REGISTER_COUNT];
+  void dump_registers() {
+    for (int i = 0; i < REGISTERS_COUNT; ++i) {
+      printf("%s = %08x\n", registers_name[i], registers[i]);
+    }
+    printf("EIP = %08x\n", eip);
+  }
+
+  uint32_t get_code8(int index) { return memory[eip + index]; }
+
+  int32_t get_sign_code8(int index) { return (int8_t)memory[eip + index]; }
+
+  uint32_t get_code32(int index) {
+    uint32_t ret = 0;
+    for (int i = 0; i < 4; ++i) {
+      ret |= get_code8(index + i) << (i * 8);
+    }
+    return ret;
+  }
+
+  void mov_r32_imm32() {
+    uint8_t reg = get_code8(0) - 0xB8;
+    uint32_t value = get_code32(1);
+    registers[reg] = value;
+    eip += 5;
+  }
+
+  void short_jump() {
+    int8_t diff = get_sign_code8(1);
+    eip += (diff + 2);
+  }
+
+  using instruction_func_t = std::function<void()>;
+  std::vector<instruction_func_t> instructions;
+  void init_instructions() {
+    instructions.reserve(256);
+    for (int i = 0; i < 8; ++i) {
+      instructions[0xB8 + i] = [this]() { mov_r32_imm32(); };
+    }
+    instructions[0xEB] = [this]() { short_jump(); };
+  }
+
+  uint32_t registers[REGISTERS_COUNT];
   uint32_t eflags;
   char* memory;
   uint32_t eip;
--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,14 @@
 #include <fstream>
 #include <iostream>
 
+static const size_t MEMORY_SIZE = 1024 * 1024;
+
 int main(int argc, char* argv[]) {
   if (argc != 2) {
     std::cerr << "usage: px86 filename" << std::endl;
     return 1;
   }
-  Emulator emu(1024, 0x0000, 0x7c00);
+  Emulator emu(MEMORY_SIZE, 0x0000, 0x7c00);
   std::ifstream file(argv[1], std::ios::binary);
   if (!file.is_open()) {
     std::cerr << "[error] failed to open file" << std::endl;
@@ -15,4 +17,19 @@ int main(int argc, char* argv[]) {
   }
 
   file.read(emu.memory, sizeof(uint8_t) * 1024);
+
+  emu.init_instructions();
+  while (emu.eip < MEMORY_SIZE) {
+    uint8_t code = emu.get_code8(0);
+    printf("EIP = %X, Code = %02X\n", emu.eip, code);
+
+    emu.instructions[code]();
+
+    if (emu.eip == 0x00) {
+      printf("\n\nend of program.\n\n");
+      break;
+    }
+  }
+
+  emu.dump_registers();
 }
